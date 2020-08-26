930ebe9877001a09657d5cffec8d4b39df0f9593
benklinton <benklinton@gmail.com>
2020-08-26 16:46:21 -0600

    changed class to className

--- soffe-doodles/src/components/navbar/index.js
+++ soffe-doodles/src/components/navbar/index.js
@@ -2,27 +2,27 @@ import React from "react";
 
 function Navbar() {
    return (
-    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="*">Start Bootstrap</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div className="container">
+      <a className="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="*">Start Bootstrap</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item active px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="index.html">Home
-              <span class="sr-only">(current)</span>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item active px-lg-4">
+            <a className="nav-link text-uppercase text-expanded" href="index.html">Home
+              <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="about.html">About</a>
+          <li className="nav-item px-lg-4">
+            <a className="nav-link text-uppercase text-expanded" href="about.html">About</a>
           </li>
-          <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="products.html">Products</a>
+          <li className="nav-item px-lg-4">
+            <a className="nav-link text-uppercase text-expanded" href="products.html">Products</a>
           </li>
-          <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="store.html">Store</a>
+          <li className="nav-item px-lg-4">
+            <a className="nav-link text-uppercase text-expanded" href="store.html">Store</a>
           </li>
         </ul>
       </div>

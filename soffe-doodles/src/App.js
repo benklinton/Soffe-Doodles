b638210e6770e1d8a288dd0818fdc4e7d55fece5
benklinton <benklinton@gmail.com>
2020-08-26 17:40:58 -0600

    created heading component

--- soffe-doodles/src/App.js
+++ soffe-doodles/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navbar from "./components/navbar"
-
+import Heading from "./components/heading"
 function App() {
   return (
     <div>
+      <Heading/>
       <Navbar/>
     </div>
   );
---
+++ soffe-doodles/src/components/heading/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Heading () {
+    return (
+        <h1 className="site-heading text-center text-white d-none d-lg-block">
+    <h2 className="site-heading-upper text-primary mb-3">Soffe Doodles</h2>
+    <p className="site-heading-lower text-primary">Our promise: We are not a puppy mill. Each of our Parent dogs are treated as family members. We keep small numbers of extraordinary dogs to breed and share the small number of puppies we sell.
+Our females are not over bred. Our home is always open to share the delightful life of raising puppies.  
+(due to covid, we will not be able to have visitors.)</p>
+</h1>
+    )
+};
+
+export default Heading;

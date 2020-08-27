d22b6037b0cf3371cf55299dcffec024095b3c32
benklinton <benklinton@gmail.com>
2020-08-27 13:47:51 -0600

    added section components

--- soffe-doodles/src/App.js
+++ soffe-doodles/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "./components/navbar";
 import Slides from "./components/carousel";
+import Section from "./components/section";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Slides/>
+      <Section/>
     </div>
   );
 }

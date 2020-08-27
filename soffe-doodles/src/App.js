e172a6b892c09392b3a24edb9c794f3b778e758f
benklinton <benklinton@gmail.com>
2020-08-27 13:12:09 -0600

    created carousel component

--- soffe-doodles/src/App.js
+++ soffe-doodles/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Navbar from "./components/navbar";
+import Circus from "./components/carousel";
+
 function App() {
   return (
     <div>
       <Navbar/>
+      <Circus/>
     </div>
   );
 }
---
+++ soffe-doodles/src/components/carousel/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './style.css';
+import Carousel from 'react-bootstrap/Carousel'
+function Circus() {
+    return (
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://via.placeholder.com/1920x1080"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://via.placeholder.com/1920x1080"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://via.placeholder.com/1920x1080"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    );
+};
+
+export default Circus;

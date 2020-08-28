e276479a8cb18503e80882d95d838f29ae41d18d
benklinton <benklinton@gmail.com>
2020-08-28 10:50:12 -0600

    added font styles

--- soffe-doodles/src/components/section/index.js
+++ soffe-doodles/src/components/section/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import './style.css';
 
 function Section() {
     return (
-        <section class="py-5">
-            <div class="container">
-                <h1 class="font-weight-light">Who We Are:</h1>
-                <p className="lead">We are making a difference in the puppy breeding world.
+        <section className="py-5">
+            <div className="container">
+                <h1 className="font-weight-light font-style-1">Who We Are:</h1>
+                <p className="lead font-2">We are making a difference in the puppy breeding world.
                 We are dedicated to raising and training incredible parent dogs so that our puppies are outstanding.
                 We work with our Parent dogs as well as our puppies everyday to insure a happy life for each of them.
                 All of our parent dogs are trained to be therapy companions so that they are ready for their new families after 2 to 3 litters.
                 Soffe Doodles will never over breed. We have a small number of outstanding dogs with calm temperaments that are passed on to their puppies.
                 If you are loooking for an outstanding puddy and its important to buy from a responisble breeder, you have come to the right place
                 </p>
-                <h1>Our Promise:</h1>
-                <p className="lead">We are not a puppy mill. Each of our Parent dogs are treated as family members.
+                <h1 className="font-style-1">Our Promise:</h1>
+                <p className="lead font-style-2">We are not a puppy mill. Each of our Parent dogs are treated as family members.
                 We keep small numbers of extraordinary dogs to breed and share the small number of puppies we sell.
                 Our females are not over bred.
                 Our home is always open to share the delightful life of raising puppies.

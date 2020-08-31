8be76706cad9c1be89291927e647154baf52e936
benklinton <benklinton@gmail.com>
2020-08-31 13:58:56 -0600

    changed text for testimonal box

--- soffe-doodles/src/components/testimonial/index.js
+++ soffe-doodles/src/components/testimonial/index.js
@@ -6,8 +6,7 @@ function Testimonial () {
         <div className="fluid-container bg-content">
             <div className="container-style col-md-8 mb-5">
                     <p>
-                        This is the best bog i ever had
-                        -Loralee
+                        Testimonal text will go here
                     </p>
             </div>
         </div>

0e6a04f142d964711773a5838addd97adcab1bf3
benklinton <benklinton@gmail.com>
2020-08-31 13:57:47 -0600

    fixed testimonal component

--- soffe-doodles/src/components/testimonial/index.js
+++ soffe-doodles/src/components/testimonial/index.js
@@ -4,7 +4,12 @@ import './style.css';
 function Testimonial () {
     return (
         <div className="fluid-container bg-content">
-            
+            <div className="container-style col-md-8 mb-5">
+                    <p>
+                        This is the best bog i ever had
+                        -Loralee
+                    </p>
+            </div>
         </div>
     )
 }

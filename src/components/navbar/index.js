1c4cfd2a5657ca4fcc034ce6c48b9f945ea59573
unknown <benklinton@gmail.com>
2020-09-09 10:15:23 -0600

    fixed navbar responsivness

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,37 +3,40 @@ import "./style.css";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import {BrowserRouter as Router,} from 'react-router-dom';
+import { BrowserRouter as Router, } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 function Navigation() {
   return (
-    <Navbar className="navbar navbar-expand-lg navbar-dark bg-custom static-top">
+    <Nav className="navbar navbar-expand-lg navbar-dark bg-custom static-top">
       <Container>
         <Navbar.Brand>
-          <img src="http://placehold.it/150x50?text=Logo" alt=""></img>
+          <img src="http://placehold.it/150x50?text=Logo" alt="Logo"></img>
         </Navbar.Brand>
+        <Button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </Button>
         <Navbar.Collapse id="navbarResponsive">
-          <Nav className="ml-auto">
+          <ul className="navbar-nav ml-auto">
             <Nav.Item>
               <Nav.Link href="/">Home
-              </Nav.Link>
+                  </Nav.Link>
             </Nav.Item>
             <Router>
-            <Nav.Item>
-             <Nav.Link href ='/gallery'>Gallery</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="">TBD</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="">TBD</Nav.Link>
-            </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/gallery">Gallery</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/">TBD</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/">TBD</Nav.Link>
+              </Nav.Item>
             </Router>
-          </Nav>
+          </ul>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+    </Nav>
   )
 }
 

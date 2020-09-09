28e8cfa4932b15239046d276e9eec19d60ea5bbb
unknown <benklinton@gmail.com>
2020-09-09 11:00:22 -0600

    fixed carousel responsiveness

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -2,45 +2,33 @@ import React from 'react';
 import './style.css';
 import Carousel from 'react-bootstrap/Carousel'
 function Slides() {
-    return (
-      <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://via.placeholder.com/1920x1080"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://via.placeholder.com/1920x1080"
-      alt="Third slide"
-    />
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src="https://via.placeholder.com/1920x1080?text=FirstSlide"
+          alt="First slide"
+        />
+      </Carousel.Item>
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://via.placeholder.com/1920x1080"
-      alt="Third slide"
-    />
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src="https://via.placeholder.com/1920x1080?text=SecondSlide"
+          alt="Third slide"
+        />
+      </Carousel.Item>
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-    );
+      <Carousel.Item>
+        <img
+          className="d-block"
+          src="https://via.placeholder.com/1920x1080?text=ThirdSlide"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default Slides;

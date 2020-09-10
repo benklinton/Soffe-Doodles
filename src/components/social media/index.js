50fd8536fc8738662f12a1b53b25db35b0add474
unknown <benklinton@gmail.com>
2020-09-10 10:55:22 -0600

    fixed social media responivness

--- src/components/social media/index.js
+++ src/components/social media/index.js
@@ -5,15 +5,15 @@ import Row from 'react-bootstrap/Row';
 
 function SocialMedia() {
     return (
-       <Container className="container-fluid content">
+       <Container className="container-fluid content mb-4">
             <h1>Follow Us On Social Media</h1>
             <Row className="d-flex justify-content-center">
             <div className=" col-12 mt-4"><img alt='' src='https://via.placeholder.com/300x300'></img></div>
             </Row>
-            <Row className="d-flex justify-content-center">
-            <a className="col-1 mt-4" href="https://www.facebook.com/soffefarm" target="_blank" rel="noopener noreferrer" ><i className="fab fa-facebook fa-4x"></i></a>
-            <a className="col-1 mt-4" href="https://www.instagram.com/soffedoodles" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram fa-4x"></i></a>
-            <a className="col-1 mt-4" href="https://www.youtube.com/channel/UCr37J-facdyDROvJrGXOdrA" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube fa-4x"></i></a>
+            <Row className="d-flex justify-content-center media">
+            <a className="flex mt-4 mx-1" href="https://www.facebook.com/soffefarm" target="_blank" rel="noopener noreferrer" ><i className="fab fa-facebook fa-4x"></i></a>
+            <a className="flex mt-4 mx-1" href="https://www.instagram.com/soffedoodles" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram fa-4x"></i></a>
+            <a className="flex mt-4 mx-1" href="https://www.youtube.com/channel/UCr37J-facdyDROvJrGXOdrA" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube fa-4x"></i></a>
             </Row>
                 
             

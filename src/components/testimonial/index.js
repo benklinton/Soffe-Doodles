84235fe6aa0553207a2974a9604ab6a64242a42f
unknown <benklinton@gmail.com>
2020-09-10 10:17:20 -0600

    fixed testimonial responivness

--- src/components/testimonial/index.js
+++ src/components/testimonial/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import './style.css';
+import Card from "react-bootstrap/Card";
 
 function Testimonial () {
     return (
-        <div className="fluid-container bg-content">
-            <div className="container-style col-md-8 mb-5">
-                    <p>
-                        Testimonal text will go here
-                    </p>
-            </div>
-        </div>
+        <Card className=" text-black custom-style">
+        <Card.Img src="https://via.placeholder.com/940x300" alt="Image" style={{display: "block"}} />
+        <Card.ImgOverlay>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Testimonial text will go here
+          </Card.Text>
+          <Card.Text>-John Doe</Card.Text>
+        </Card.ImgOverlay>
+      </Card>
     )
 }
 

c6b08994d4fca8f4d07a99f0d4113cb47ec4731e
unknown <benklinton@gmail.com>
2020-09-21 15:17:15 -0600

    fixed responivenes for application page

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -5,11 +5,11 @@ import './style.css';
 
 function FormLayout() {
     return (
-        <Container>
+        <Container className="contain">
             <div>
                 <h1 className='mt-4 mb-4'>Please fill out the application</h1>
             </div>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeyRRZ1KzUw_C5csh0RHAUk5dPEdAkdyJH5Viey_SEpAwXdTw/viewform?embedded=true" width="640" height="1170" frameborder="0" marginheight="0" marginwidth="0" className="frame">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeyRRZ1KzUw_C5csh0RHAUk5dPEdAkdyJH5Viey_SEpAwXdTw/viewform?embedded=true" width="640" height="1170" frameborder="0" marginheight="0" marginwidth="0" id="frame" xs={'auto'}>Loading…</iframe>
         </Container>
     )
 }

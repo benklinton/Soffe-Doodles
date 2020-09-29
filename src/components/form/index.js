08cabdaca220d83509b2da7629ce865c8191554c
unknown <benklinton@gmail.com>
2020-09-29 17:57:37 -0600

    removed reduent code/gave iframe title

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button'
 import './style.css';
 
 function FormLayout() {
@@ -9,7 +8,7 @@ function FormLayout() {
             <div>
                 <h1 className='mt-4 mb-4'>Please fill out the application</h1>
             </div>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeyRRZ1KzUw_C5csh0RHAUk5dPEdAkdyJH5Viey_SEpAwXdTw/viewform?embedded=true" width="640" height="1170" frameborder="0" marginheight="0" marginwidth="0" id="frame" xs={'auto'}>Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeyRRZ1KzUw_C5csh0RHAUk5dPEdAkdyJH5Viey_SEpAwXdTw/viewform?embedded=true" width="640" height="1170" frameborder="0" marginheight="0" marginwidth="0" id="frame" xs={'auto'} title='Application'>Loading…</iframe>
         </Container>
     )
 }

5d395fbf173f88d0d1f1820eff41962833927a85
unknown <benklinton@gmail.com>
2020-09-29 12:32:55 -0600

    added jsx parent tags

--- src/pages/Health.js
+++ src/pages/Health.js
@@ -1,12 +1,12 @@
-import { render } from '@testing-library/react';
 import React from 'react';
-import Footer from '../components/footer';
 import Navigation from '../components/navbar';
 
 
 function Health () {
     return (
+        <>
         <Navigation/>
+        </>
     )
 }
 

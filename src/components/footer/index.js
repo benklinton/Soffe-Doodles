482160d4fafafde80d6f5e76f2212f6f1ebd8b23
unknown <benklinton@gmail.com>
2020-09-30 13:16:06 -0600

    changed footer color

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 function Footer() {
     return (
-        <footer className="py-4 bg-dark text-white-50">
+        <footer className="py-4 bg-footer text-white-50">
         <div className="container text-center">
           <small>Copyright &copy; Soffe Doodles</small>
           <div><small>Created by Blue Steel Web Design</small></div>

75f93a837265e7b3a9ef61e3179b7a358f7cb404
benklinton <benklinton@gmail.com>
2020-10-14 12:23:31 -0600

    center aligned text for form component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -6,7 +6,7 @@ function FormLayout() {
     return (
         <Container className="contain">
             <div>
-                <h1 className='mt-4 mb-4'>Please fill out the application</h1>
+                <h1 className='mt-4 mb-4' style={{textAlign:'center'}}>Please fill out the application</h1>
             </div>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeyRRZ1KzUw_C5csh0RHAUk5dPEdAkdyJH5Viey_SEpAwXdTw/viewform?embedded=true" width="640" height="1170" frameborder="0" marginheight="0" marginwidth="0" id="frame" xs={'auto'} title='Application'>Loading…</iframe>
         </Container>

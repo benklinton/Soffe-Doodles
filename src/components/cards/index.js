85bab3991d98473a5c2deafc787b021200d4ca32
benklinton <benklinton@gmail.com>
2020-10-18 18:04:14 -0600

    added images to cards component

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -4,7 +4,10 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import CardDeck from 'react-bootstrap/CardDeck'
+import CardDeck from 'react-bootstrap/CardDeck';
+import Puppy1 from './images/Puppy1.jpg';
+import Puppy2 from './images/Puppies.jpg';
+import Puppy3 from './images/Puppy2.jpg';
 
 function Cards() {
     return (
@@ -12,29 +15,29 @@ function Cards() {
             <Row>
                 <CardDeck>
                 <Card className="card-style">
-                    <Card.Img variant="top" src="https://via.placeholder.com/200x200" />
+                    <Card.Img variant="top" src={Puppy1} />
                     <Card.Body>
                         <Card.Title>Application</Card.Title>
                         <Card.Text>
                             Interested in owning a puppy? Fill out our application
                         </Card.Text>
-                        <Button variant="outline-info">Click Here</Button>
+                        <Button variant="outline-info" href="/application">Click Here</Button>
                     </Card.Body>
                 </Card>
 
                 <Card className="card-style">
-                    <Card.Img variant="top" src="https://via.placeholder.com/200x200" />
+                    <Card.Img variant="top" src={Puppy2} />
                     <Card.Body>
                         <Card.Title>Available Puppies</Card.Title>
                         <Card.Text>
-                            Check out our avilable puppies page for this season's litter
+                            Check out our available puppies page for this season's litter
                         </Card.Text>
                         <Button variant="outline-info" href='/avaliable-puppies'>Click Here</Button>
                     </Card.Body>
                 </Card>
 
                 <Card className="card-style">
-                    <Card.Img variant="top" src="https://via.placeholder.com/200x200" />
+                    <Card.Img variant="top" src={Puppy3} />
                     <Card.Body>
                         <Card.Title>Contact Us</Card.Title>
                         <Card.Text>

f90c80f9f3682a23c70df73d7a6d375387241de4
benklinton <benklinton@gmail.com>
2020-10-18 19:58:46 -0600

    added image to social media component

--- src/components/social media/index.js
+++ src/components/social media/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './style.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Image from './images/Dog.jpg';
 
 function SocialMedia() {
     return (
        <Container className="container-fluid content mb-4">
             <h1>Follow Us On Social Media</h1>
             <Row className="d-flex justify-content-center">
-            <div className=" col-12 mt-4"><img alt='' src='https://via.placeholder.com/300x300'></img></div>
+            <div className=" col-12 mt-4"><img alt='' src={Image}></img></div>
             </Row>
             <Row className="d-flex justify-content-center media">
             <a className="flex mt-4 mx-1" href="https://www.facebook.com/soffefarm" target="_blank" rel="noopener noreferrer" ><i className="fab fa-facebook fa-4x"></i></a>

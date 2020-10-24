92b897483170e6573703eb851e7e49a8ce23f319
benklinton <benklinton@gmail.com>
2020-10-24 09:48:05 -0600

    added responive margins to section component

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -10,8 +10,8 @@ function Section() {
         <div className="py-5">
             <Container className="col-md-8 mb-5 mt-5">
                 <Row>
-                    <h1 className="font-1">Who We Are:</h1>
-                    <p className="lead font-2">We are making a difference in the puppy breeding world.
+                    <h1 className="font-1 media">Who We Are:</h1>
+                    <p className="lead font-2 media">We are making a difference in the puppy breeding world.
                     We are dedicated to raising and training incredible parent dogs so that our puppies are outstanding.
                     We work with our Parent dogs as well as our puppies everyday to insure a happy life for each of them.
                     All of our parent dogs are trained to be therapy companions so that they are ready for their new families after 2 to 3 litters.
@@ -19,14 +19,14 @@ function Section() {
                     If you are loooking for an outstanding puddy and its important to buy from a responisble breeder, you have come to the right place
                     </p>
 
-                    <div className="img-style">
+                    <div className="media">
                         <img src={Doggies} className= "img-style" alt=""></img>
                     </div>
 
                 </Row>
                 <Row>
-                    <h1 className="font-1">Our Promise:</h1>
-                    <p className="lead font-2">We are not a puppy mill. Each of our Parent dogs are treated as family members.
+                    <h1 className="font-1 media">Our Promise:</h1>
+                    <p className="lead font-2 media">We are not a puppy mill. Each of our Parent dogs are treated as family members.
                     We keep small numbers of extraordinary dogs to breed and share the small number of puppies we sell.
                     Our females are not over bred.
                     Our home is always open to share the delightful life of raising puppies.

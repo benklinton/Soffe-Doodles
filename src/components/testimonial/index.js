ddd49d0889b83c535c04925ce5d6ce6b9ec425e5
benklinton <benklinton@gmail.com>
2020-10-26 14:04:42 -0600

    added mobile responiveness to testimonial section

--- src/components/testimonial/index.js
+++ src/components/testimonial/index.js
@@ -6,13 +6,13 @@ import Puppy from './images/Puppy.jpg';
 function Testimonial () {
     return (
         <Card className=" text-black custom-style">
-        <Card.Img src={Puppy} alt="image of puppy" style={{height: "0%"}}/>
+        <Card.Img src={Puppy} alt="image of puppy"/>
         <Card.ImgOverlay>
           <Card.Title className="font-1 custom-color font-size">See What People Are Saying</Card.Title>
-          <Card.Text className='font-2 custom-color'>
+          {/* <Card.Text className='font-2 custom-color'>
             Testimonial text will go here
           </Card.Text>
-          <Card.Text className='font-2 custom-color'>-John Doe</Card.Text>
+          <Card.Text className='font-2 custom-color'>-John Doe</Card.Text> */}
         </Card.ImgOverlay>
       </Card>
     )

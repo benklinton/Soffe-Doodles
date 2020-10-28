ecd1f8b77528fb317eb42acb014724d61f9e7986
benklinton <benklinton@gmail.com>
2020-10-28 11:46:17 -0600

    created contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/navbar';
+import Contact from '../components/contact';
+import Footer from "../components/footer";
+
+function ContactPage() {
+    return (
+    <>
+    <Navigation/>
+    <Contact/>
+    <Footer/>
+    </>
+    )
+}
+
+export default ContactPage;

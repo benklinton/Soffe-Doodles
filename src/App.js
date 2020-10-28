21536b9bc2a9fef664ef2c7186349cf9007c61de
benklinton <benklinton@gmail.com>
2020-10-28 11:47:29 -0600

    created route for contact page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Sires_and_Dames from './pages/Sires_and_Dames';
 import Litters from './pages/Litters';
 import Health from './pages/Health';
+import ContactPage from './pages/Contact';
 
 function App() {
     return(
@@ -19,6 +20,7 @@ function App() {
                     <Route path = '/sires-dames' component={Sires_and_Dames}/>
                     <Route path = '/avaliable-puppies' component={Litters}/>
                     <Route path = '/health' component={Health}/>
+                    <Route path = '/contact' component={ContactPage}/>
                 </Switch>
             </Router>
         </>
